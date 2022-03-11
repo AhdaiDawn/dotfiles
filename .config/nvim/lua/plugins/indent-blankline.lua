cf9e11ed1ed4765e812251bb01e93196e898262c
ahdai <ahdai19970918@gmail.com>
2022-03-11 14:25:25 +0800

    nvim: indent blankline config

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -5,9 +5,11 @@
 -- Plugin: indent-blankline
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
 
 require('indent_blankline').setup {
-  char = "▏",
+  space_char_blankline = " ",
   show_first_indent_level = false,
   filetype_exclude = {
     'help',
