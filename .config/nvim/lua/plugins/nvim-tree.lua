e8383f0ba61fe69bae5236ecf07d1df855b8b5cd
ahdai <ahdai19970918@gmail.com>
2022-03-27 13:26:58 +0800

    nvim: adjust config loader

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -12,32 +12,36 @@
 --- setup function:
 --- https://github.com/kyazdani42/nvim-tree.lua#setup
 --- See: `help NvimTree`
-local g = vim.g
+local M = {}
+function M.setup()
+  local g = vim.g
+  -- g.nvim_tree_gitignore = 1
+  g.nvim_tree_indent_markers = 1
+  g.nvim_tree_git_hl = 1
+  g.nvim_tree_highlight_opened_files = 1
+  g.nvim_tree_respect_buf_cwd = 1
+  g.nvim_tree_width_allow_resize  = 1
+  g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1
+  }
 
--- g.nvim_tree_gitignore = 1
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_respect_buf_cwd = 1
-g.nvim_tree_width_allow_resize  = 1
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1
-}
+  g.nvim_tree_icons = {
+  	default = "‣ "
+  }
 
-g.nvim_tree_icons = {
-	default = "‣ "
-}
+  require('nvim-tree').setup {
+    open_on_setup = true,
+    view = {
+      width = 32,
+      auto_resize = true
+    },
+    filters = {
+      dotfiles = true,
+      custom = { '.git', 'node_modules', '.cache', '.bin' },
+    },
+  }
+end
 
-require('nvim-tree').setup {
-  open_on_setup = true,
-  view = {
-    width = 32,
-    auto_resize = true
-  },
-  filters = {
-    dotfiles = true,
-    custom = { '.git', 'node_modules', '.cache', '.bin' },
-  },
-}
+return M
