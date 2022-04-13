4594c9d529763f9cfb571d7f3bae6aac04de487f
ahdai <ahdai19970918@gmail.com>
2022-04-13 22:00:41 +0800

    nvim: move discard api

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -16,7 +16,7 @@ local M = {}
 function M.setup()
   local g = vim.g
   -- g.nvim_tree_gitignore = 1
-  g.nvim_tree_indent_markers = 1
+  -- g.nvim_tree_indent_markers = 1
   g.nvim_tree_git_hl = 1
   g.nvim_tree_highlight_opened_files = 1
   g.nvim_tree_respect_buf_cwd = 1
