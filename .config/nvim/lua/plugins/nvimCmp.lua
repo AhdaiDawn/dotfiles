2785d2b50d638735f5ba2228ca452f0f5aca3c8a
ahdai <ahdai19970918@gmail.com>
2022-06-03 21:28:33 +0800

    nvim: format code

--- .config/nvim/lua/plugins/nvimCmp.lua
+++ .config/nvim/lua/plugins/nvimCmp.lua
@@ -8,71 +8,71 @@
 local present, cmp = pcall(require, "cmp")
 
 if not present then
-   return
+  return
 end
 
 vim.opt.completeopt = "menuone,noselect"
 
 local function border(hl_name)
-   return {
-      { "╭", hl_name },
-      { "─", hl_name },
-      { "╮", hl_name },
-      { "│", hl_name },
-      { "╯", hl_name },
-      { "─", hl_name },
-      { "╰", hl_name },
-      { "│", hl_name },
-   }
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
 end
 
 local cmp_window = require "cmp.utils.window"
 
 function cmp_window:has_scrollbar()
-   return false
+  return false
 end
 
 local options = {
-   window = {
-      completion = {
-         border = border "CmpBorder",
-      },
-      documentation = {
-         border = border "CmpDocBorder",
-      },
-   },
-   snippet = {
-      expand = function(args)
-         require("luasnip").lsp_expand(args.body)
-      end,
-   },
-   mapping = {
-      ["<C-p>"] = cmp.mapping.select_prev_item(),
-      ["<C-n>"] = cmp.mapping.select_next_item(),
-      -- ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-      -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.close(),
-      ["<CR>"] = cmp.mapping.confirm { select = true },
-      ["<Tab>"] = cmp.mapping.confirm { select = true },
-      ["<c-l>"] = cmp.mapping(function(fallback)
-         if require("luasnip").expand_or_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-         else
-            fallback()
-         end
-      end, {
-         "i",
-         "s",
-      }),
-   },
-   sources = {
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-      { name = "buffer" },
-      { name = "nvim_lua" },
-      { name = "path" },
-   },
+  window = {
+    completion = {
+      border = border "CmpBorder",
+    },
+    documentation = {
+      border = border "CmpDocBorder",
+    },
+  },
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    -- ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<Tab>"] = cmp.mapping.confirm { select = true },
+    ["<c-l>"] = cmp.mapping(function(fallback)
+      if require("luasnip").expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "nvim_lua" },
+    { name = "path" },
+  },
 }
 
 cmp.setup(options)
