6c62121ac9ee0fd386dd13023011ecbe6756629c
ahdai <ahdai19970918@gmail.com>
2022-06-14 14:19:35 +0800

    nvim: fix oscyank not work with tmux

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -77,4 +77,5 @@ vim.defer_fn(function()
 
   -- 使用OSC yank
   cmd [[autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif]]
+  vim.g['oscyank_term'] = 'default'
 end, 0)
