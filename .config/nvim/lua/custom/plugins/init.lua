ced67316c366f4797cf0d51bda5b6a66c4236b2c
ahdai <ahdai19970918@gmail.com>
2022-11-07 19:36:30 +0800

    nvim: add plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -18,5 +18,16 @@ return {
       require "plugins.configs.lspconfig"
       require "custom.plugins.lspconfig"
     end
+  },
+
+  ["justinmk/vim-sneak"] = {},
+
+  ["kylechui/nvim-surround"]= {
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
   }
 }
