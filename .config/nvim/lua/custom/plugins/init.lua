45cb2bea451979e91ff4a22356cf4773c096ae62
ahdai <ahdai19970918@gmail.com>
2022-11-15 21:23:34 +0800

    nvim: surround

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -22,12 +22,5 @@ return {
 
   ["justinmk/vim-sneak"] = {},
 
-  ["kylechui/nvim-surround"]= {
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-  }
+  ["kylechui/nvim-surround"]= {},
 }
