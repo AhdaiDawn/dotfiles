d29977f619c72940a7737deb3949610182aeba2e
ahdai <ahdai19970918@gmail.com>
2023-06-09 00:35:57 +0800

    nvim: lsp

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -2,52 +2,23 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-
--- List of servers to install
 local servers = { "clangd" }
 
-require("mason-lspconfig").setup({
-  ensure_installed = servers,
-})
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
 
--- This will setup lsp for servers you listed above
--- And servers you install through mason UI
--- So defining servers in the list above is optional
-require("mason-lspconfig").setup_handlers({
-  -- Default setup for all servers, unless a custom one is defined below
-  function(server_name)
-    lspconfig[server_name].setup({
-      on_attach = function(client, bufnr)
-        on_attach(client, bufnr)
-        -- Add your other things here
-        -- Example being format on save or something
-      end,
-      capabilities = capabilities,
-    })
-  end,
-  -- custom setup for a server goes after the function above
-  -- Example, override lua_ls
-  ["lua_ls"] = function()
-    lspconfig["lua_ls"].setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-              [vim.fn.stdpath("data") .. "/lazy/extensions/nvchad_types"] = true,
-              [vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
-            },
-            maxPreload = 100000,
-            preloadFileSize = 10000,
-          },
-        }
-      }
-    })
-  end,
-})
+-- Without the loop, you would have to manually set up each LSP
+--
+-- lspconfig.html.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+--
+-- lspconfig.cssls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,7 +1,1 @@
 local cmd = vim.cmd
-
--- vim.defer_fn(function()
---   -- 使用OSC yank
---   cmd [[autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankRegister "' | endif]]
---   vim.g["oscyank_term"] = "default"
--- end, 0)
