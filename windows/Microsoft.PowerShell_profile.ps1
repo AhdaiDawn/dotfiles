5fe0f94a4caf538a6b45a7419912e4d332ad6a0d
Ahdai <ahdai19970918@gmail.com>
2024-10-24 22:58:56 +0800

    ps: keymap

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,8 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete # 设置 Tab 键补全
 # Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function MenuComplete # 设置 Ctrl+d 为菜单补全和 Intellisense
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
+Set-PSReadLineKeyHandler -Chord Ctrl-a -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord Ctrl-e -Function EndOfLine
 
 # 设置提示符
 function prompt {
