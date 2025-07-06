0b362d85d6497e70046970bd87a4f229b921576a
ahdai <ahdai19970918@gmail.com>
2025-07-06 11:58:18 +0800

    nvim: plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,14 +5,14 @@ local autocmd = vim.api.nvim_create_autocmd
 -- Options
 ------------------------------
 vim.g.mapleader = " "
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 vim.o.showmode = false
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus' -- -- Sync clipboard between OS and Neovim.
+  vim.o.clipboard = "unnamedplus" -- -- Sync clipboard between OS and Neovim.
 end)
-vim.o.signcolumn = 'yes' -- Keep signcolumn on by default
+vim.o.signcolumn = "yes" -- Keep signcolumn on by default
 vim.o.updatetime = 250 -- Decrease update time
 vim.o.timeoutlen = 400 -- Decrease mapped sequence wait time
 vim.o.breakindent = true -- Enable break indent
@@ -45,15 +45,17 @@ vim.o.splitbelow = true -- split go below
 vim.o.splitright = true -- vertical split to the right
 vim.o.termguicolors = true -- terminal gui colors
 vim.o.background = "dark" -- use dark theme only
-vim.cmd('filetype plugin on') -- set filetype
+vim.cmd "filetype plugin on" -- set filetype
 vim.o.list = true -- Sets how neovim will display certain whitespace characters in the editor.
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.o.inccommand = 'split' -- Preview substitutions live, as you type!
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.o.inccommand = "split" -- Preview substitutions live, as you type!
 -- Undo
-vim.opt.undodir = vim.fn.stdpath("data") .. "/nvim/undo"
-vim.opt.undofile = true-- clean plugins
+vim.opt.undodir = vim.fn.stdpath "data" .. "/nvim/undo"
+vim.opt.undofile = true -- clean plugins
 
 -- diasble build-in plugin
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
 vim.g.loaded_remote_plugins = 1
@@ -112,7 +114,7 @@ map("n", "<S-k>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<S-j>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 map("n", "<leader>bd", "<Cmd>:bd<CR>", { desc = "Delete Buffer and Window" })
 
-map('n', '<leader>qf', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map("n", "<leader>qf", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Clipboard
 -- paste text but DONT copy the overridden text
@@ -139,17 +141,17 @@ map("n", "<leader>qq", ":q<CR>", { desc = "Quit buffer without save" })
 map("n", "<leader>qa", ":qa<CR>", { desc = "Quit all buffer with save" })
 
 -- Abbrev
-vim.cmd("cnoreabbrev Q  q")
-vim.cmd("cnoreabbrev q1  q!")
-vim.cmd("cnoreabbrev Q1  q!")
-vim.cmd("cnoreabbrev Qa1 qa!")
-vim.cmd("cnoreabbrev Qa qa")
-vim.cmd("cnoreabbrev W  w")
-vim.cmd("cnoreabbrev Wq wq")
-vim.cmd("cnoreabbrev WQ wq")
-vim.cmd("cnoreabbrev Set set")
-vim.cmd("cnoreabbrev SEt set")
-vim.cmd("cnoreabbrev SET set")
+vim.cmd "cnoreabbrev Q  q"
+vim.cmd "cnoreabbrev q1  q!"
+vim.cmd "cnoreabbrev Q1  q!"
+vim.cmd "cnoreabbrev Qa1 qa!"
+vim.cmd "cnoreabbrev Qa qa"
+vim.cmd "cnoreabbrev W  w"
+vim.cmd "cnoreabbrev Wq wq"
+vim.cmd "cnoreabbrev WQ wq"
+vim.cmd "cnoreabbrev Set set"
+vim.cmd "cnoreabbrev SEt set"
+vim.cmd "cnoreabbrev SET set"
 
 -- AutoCOMMANDS
 ------------------------------
@@ -157,16 +159,16 @@ local group = vim.api.nvim_create_augroup("user_cmds", { clear = true })
 
 -- Mode based Cursorline
 autocmd("InsertEnter", {
-    pattern = "*",
-    callback = function()
-        vim.o.cursorline = false
-    end,
+  pattern = "*",
+  callback = function()
+    vim.o.cursorline = false
+  end,
 })
 autocmd("InsertLeave", {
-    pattern = "*",
-    callback = function()
-        vim.o.cursorline = true
-    end,
+  pattern = "*",
+  callback = function()
+    vim.o.cursorline = true
+  end,
 })
 
 -- 恢复光标位置
@@ -187,20 +189,20 @@ autocmd("BufReadPost", {
 
 -- Auto Hlsearch
 vim.on_key(function(char)
-    if vim.fn.mode(1) == "n" then
-        local new_hlsearch = vim.iter({ "<CR>", "n", "N", "*", "#", "?", "/" }):find(vim.fn.keytrans(char)) ~= nil
-        if vim.opt.hlsearch:get() ~= new_hlsearch then
-            vim.opt.hlsearch = new_hlsearch
-        end
+  if vim.fn.mode(1) == "n" then
+    local new_hlsearch = vim.iter({ "<CR>", "n", "N", "*", "#", "?", "/" }):find(vim.fn.keytrans(char)) ~= nil
+    if vim.opt.hlsearch:get() ~= new_hlsearch then
+      vim.opt.hlsearch = new_hlsearch
     end
-end, vim.api.nvim_create_namespace("auto_hlsearch"))
+  end
+end, vim.api.nvim_create_namespace "auto_hlsearch")
 
 ------------------------------
 -- Completion from :h ins-completion
 vim.opt.omnifunc = "syntaxcomplete#Complete" -- Auto Completion - Enable Omni complete features
-vim.cmd("set complete+=k") -- Enable Spelling Suggestions for Auto-Completion:
+vim.cmd "set complete+=k" -- Enable Spelling Suggestions for Auto-Completion:
 vim.opt.completeopt = { "menu", "menuone", "noinsert" }
-vim.cmd([[
+vim.cmd [[
 " Minimalist-Tab Complete
     inoremap <expr> <Tab> TabComplete()
     fun! TabComplete()
@@ -226,70 +228,140 @@ vim.cmd([[
             call feedkeys("\<C-N>", 'n')
         end
     endfun
-]])
+]]
 
--- Automatically Pair brackets, parethesis, and quotes
-map("i", "'", "''<left>")
-map("i", '"', '""<left>')
-map("i", "(", "()<left>")
-map("i", "[", "[]<left>")
-map("i", "{", "{}<left>")
-map("i", "{;", "{};<left><left>")
-map("i", "/*", "/**/<left><left>")
+if not enable_plugin then
+  -- Automatically Pair brackets, parethesis, and quotes
+  map("i", "'", "''<left>")
+  map("i", '"', '""<left>')
+  map("i", "(", "()<left>")
+  map("i", "[", "[]<left>")
+  map("i", "{", "{}<left>")
+  map("i", "{;", "{};<left><left>")
+  map("i", "/*", "/**/<left><left>")
+end
 
 --------------------------------------------
 --- Plugin
 --------------------------------------------
 if enable_plugin then
-    local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-    if not (vim.uv or vim.loop).fs_stat(lazypath) then
-        local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-        local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-        if vim.v.shell_error ~= 0 then
-            error('Error cloning lazy.nvim:\n' .. out)
-        end
+  local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+      error("Error cloning lazy.nvim:\n" .. out)
     end
+  end
 
-    local rtp = vim.opt.rtp
-    rtp:prepend(lazypath)
+  local rtp = vim.opt.rtp
+  rtp:prepend(lazypath)
 
-    require('lazy').setup({
-        { 
-            "ellisonleao/gruvbox.nvim" ,
-            config = function()
-                vim.cmd([[colorscheme gruvbox]])
-            end,
-        },
+  require("lazy").setup({
+    {
+      "ellisonleao/gruvbox.nvim",
+      config = function()
+        vim.cmd [[colorscheme gruvbox]]
+      end,
+    },
+    {
+      "nvim-tree/nvim-tree.lua",
+      cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+      keys = {
+        { "<A-e>", "<cmd>NvimTreeToggle<cr>", desc = "NvimTreeToggle" },
+      },
+      config = function(_, opts)
+        require("nvim-tree").setup(opts)
+      end,
+    },
+    {
+      "windwp/nvim-autopairs",
+      config = function(_, opts)
+        require("nvim-autopairs").setup(opts)
+      end,
+    },
+    {
+      "ibhagwan/fzf-lua",
+      keys = {
+        { "<leader>p", "<cmd>FzfLua<cr>", desc = "fzf" },
+        { "<leader>o", "<cmd>FzfLua files<cr>", desc = "fzf files" },
+        { "<leader>f", "<cmd>FzfLua live_grep<cr>", desc = "fzf live_grep" },
+        { "<leader>b", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+        { "<leader>r", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
+        { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
+        { "<leader>m", "<cmd>FzfLua marks<cr>", desc = "marks" },
+        { "<leader>j", "<cmd>FzfLua jumps<cr>", desc = "jumps" },
+        { "<leader>t", "<cmd>FzfLua tags<cr>", desc = "tags" },
         {
-            "ibhagwan/fzf-lua",
-            keys = {
-                { "<leader>;", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
-                { "<leader>f", "<cmd>FzfLua files<cr>", desc = "fzf files" },
-                { "<leader>b", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-                { "<leader>r", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
-                { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
-                { "<leader>m", "<cmd>FzfLua marks<cr>", desc = "marks" },
-            },
+          "gd",
+          function()
+            if #vim.fn.tagfiles() > 0 then
+              require("fzf-lua").tags_grep_cword()
+            else
+              require("fzf-lua").grep_cword()
+            end
+          end,
+          desc = "FZF Smart Definition (Tags or Grep)",
         },
-    }, {
-        ui = {
-            icons = vim.g.have_nerd_font and {} or {
-                cmd = '⌘',
-                config = '🛠',
-                event = '📅',
-                ft = '📂',
-                init = '⚙',
-                keys = '🗝',
-                plugin = '🔌',
-                runtime = '💻',
-                require = '🌙',
-                source = '📄',
-                start = '🚀',
-                task = '📌',
-                lazy = '💤 ',
-            },
+        { "gw", "<cmd>FzfLua grep_cword<cr>", desc = "FZF word (under cursor)" },
+      },
+      opts = function(_, opts)
+        return {
+          winopts = {
+            width = 1.0,
+            height = 1.0,
+            row = 0.5,
+            col = 0.5,
+          },
+        }
+      end,
+    },
+    {
+      "ludovicchabant/vim-gutentags",
+      lazy = false,
+      config = function()
+        vim.g.gutentags_project_root = { ".git", ".svn", ".hg", ".project", ".root" }
+        vim.g.gutentags_ctags_tagfile = ".tags"
+      end,
+    },
+    {
+      -- formatting!
+      "stevearc/conform.nvim",
+      opts = {
+        formatters_by_ft = {
+          lua = { "stylua" },
+          cpp = { "clang-format" },
         },
-    })
+      },
+      keys = {
+        {
+          "<leader>fm",
+          function()
+            require("conform").format { lsp_fallback = true }
+          end,
+          desc = "format",
+        },
+      },
+    },
+  }, {
+    ui = {
+      icons = vim.g.have_nerd_font and {} or {
+        cmd = "⌘",
+        config = "🛠",
+        event = "📅",
+        ft = "📂",
+        init = "⚙",
+        keys = "🗝",
+        plugin = "🔌",
+        runtime = "💻",
+        require = "🌙",
+        source = "📄",
+        start = "🚀",
+        task = "📌",
+        lazy = "💤 ",
+      },
+    },
+  })
 end
 --------------------------------------------
 --- Plugin end
