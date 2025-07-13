46e4c136345382a5b0789bc0a763a52c848cf880
ahdai <ahdai19970918@gmail.com>
2025-07-13 15:01:52 +0800

    nvim: leap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -366,6 +366,8 @@ if enable_plugin then
       config = function(_, opts)
         local leap = require "leap"
         leap.add_default_mappings(true)
+        vim.keymap.del({ "x", "o" }, "x")
+        vim.keymap.del({ "x", "o" }, "X")
       end,
     },
     {
