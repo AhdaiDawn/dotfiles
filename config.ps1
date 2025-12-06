df656231c1558139c81840ed7bd3904473a8dfe2
ahdai <ahdai19970918@gmail.com>
2025-12-06 17:17:46 +0800

    pwsh: config

---
+++ config.ps1
@@ -0,0 +1,24 @@
+function ToAbsolutePath {
+    param (
+        [string]$RelativePath
+    )
+
+    $configPath = Join-Path $PWD $RelativePath
+    $resolvedPath = Resolve-Path $configPath -ErrorAction SilentlyContinue
+
+    if ($resolvedPath) {
+        return $resolvedPath.Path
+    } else {
+        Write-Error "路径不存在: $configPath"
+        return $null
+    }
+}
+
+# powershell profile
+New-Item -Force -ItemType SymbolicLink -Path $PROFILE -Target (ToAbsolutePath "windows\Microsoft.PowerShell_profile.ps1")
+
+# z.lua
+New-Item -Force -ItemType SymbolicLink -Path (Join-Path (Split-Path -parent $PROFILE) "z.lua") -Target (ToAbsolutePath "tools\z.lua")
+
+# vsvimrc
+New-Item -Force -ItemType SymbolicLink -Path "~\_vsvimrc" -Target (ToAbsolutePath "windows\_vsvimrc")
