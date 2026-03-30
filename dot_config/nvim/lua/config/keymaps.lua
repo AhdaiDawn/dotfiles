07005590e1da5ba0c8ad18515129179aa5dd2d1c
ahdai <ahdai19970918@gmail.com>
2026-03-30 22:53:14 +0800

    config

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,7 @@ local map = vim.keymap.set
 local unmap = vim.keymap.del
 
 unmap("n", "<leader>l")
+unmap({ "n", "i", "v" }, "<A-j>")
+unmap({ "n", "i", "v" }, "<A-k>")
 
 map("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save file" })
