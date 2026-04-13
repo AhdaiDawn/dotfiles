95aa4ef21c3fc55b04000aa48bff66fa00c61be5
ahdai <ahdai19970918@gmail.com>
2026-04-13 13:46:18 +0800

    nvim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@ local opt = vim.opt
 opt.smoothscroll = false
 opt.relativenumber = false
 vim.g.autoformat = false
+vim.o.exrc = true
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -19,6 +19,16 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = { enabled = false },
+      picker = {
+        hidden = true,
+        -- ignored = true,
+        sources = {
+          files = {
+            hidden = true,
+            -- ignored = true,
+          },
+        },
+      },
     },
     keys = {
       -- find
