4ef1dc867c74e61982f92e682b6f6e167bb4936a
ahdai <ahdai19970918@gmail.com>
2026-06-16 21:30:44 +0800

    update

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,5 +1,6 @@
 local opt = vim.opt
 opt.smoothscroll = false
 opt.relativenumber = false
+opt.wrap = true
 vim.g.autoformat = false
 vim.o.exrc = true
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -34,13 +34,6 @@ return {
       -- find
       { "<leader>o", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
       { "<leader>r", LazyVim.pick("oldfiles"), desc = "Recent" },
-      {
-        "<A-e>",
-        function()
-          Snacks.explorer({ cwd = LazyVim.root() })
-        end,
-        desc = "Explorer Snacks (root dir)",
-      },
     },
   },
   {
