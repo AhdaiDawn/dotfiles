b311fad6ee580c396af56f52501053122a00c692
ahdai <ahdai19970918@gmail.com>
2026-09-03 15:09:57 +0800

    fix(nvim): tolerate missing LazyVim defaults

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 local map = vim.keymap.set
-local unmap = vim.keymap.del
-
-unmap("n", "<leader>l")
-unmap({ "n", "i", "v" }, "<A-j>")
-unmap({ "n", "i", "v" }, "<A-k>")
+pcall(vim.keymap.del, "n", "<leader>l")
+pcall(vim.keymap.del, { "n", "i", "v" }, "<A-j>")
+pcall(vim.keymap.del, { "n", "i", "v" }, "<A-k>")
 
 map("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save file" })
